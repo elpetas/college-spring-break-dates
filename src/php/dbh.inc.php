10a8cd9f752685f2168e6a715c0e501d2b6df4f6
elpetas <69687390+elpetas@users.noreply.github.com>
2023-02-28 03:21:07 -0700

    hide password

--- src/php/dbh.inc.php
+++ src/php/dbh.inc.php
@@ -1,7 +1,7 @@
 <?php
 $dbServername="us-cdbr-east-06.cleardb.net";
 $dbUsername="bac890cd1d657c";
-$dbPassword="3f3da4cc";
+$dbPassword="------";
 $dbName="heroku_4050c3bcd543143";
 $conn =mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
 
